825175708c846af8b0cd106d6739d7fb7514838d
anders130 <93037023+anders130@users.noreply.github.com>
2025-12-29 13:47:36 +0100

    make project work with nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+    inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+    outputs = {nixpkgs, ...}: let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                dotnet-sdk
+            ];
+        };
+    };
+}
